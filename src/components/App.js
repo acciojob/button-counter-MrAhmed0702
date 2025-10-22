6967206abcd57c068fc92e5904c88131f7da9acf
Ahmed Mohammed Jayanti Mochi <amochi670@rku.ac.in>
2025-10-22 23:14:58 +0530

    Add state and increment function to App component

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,16 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const ["click", "SetClick"] = useState(0);
+
+  function increment(){
+    return SetClick + 1;
+  }
+  
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>Button clicked {click} times</p>
+      <button type="submit" onclick={increment}>Increment</button>
     </div>
   )
 }
