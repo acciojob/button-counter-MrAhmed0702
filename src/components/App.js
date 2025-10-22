3acf277a4d1a9bc50f8250159a90ed4ef37742ad
Ahmed Mohammed Jayanti Mochi <amochi670@rku.ac.in>
2025-10-22 23:29:30 +0530

    Inline setClick in button onClick handler
    Refactor increment function to inline setClick

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,11 @@ import './../styles/App.css';
 
 const App = () => {
   const [click, setClick] = useState(0);
-
-  function increment(){
-    return SetClick(click + 1);
-  }
   
   return (
     <div>
       <p>Button clicked {click} times</p>
-      <button onClick={increment}>Increment</button>
+      <button onClick={()=>setClick(click+1)}>Increment</button>
     </div>
   )
 }
